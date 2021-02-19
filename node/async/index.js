ca6dcc0c17c287c82705167f186adb8aa11132f6
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2021-02-19 17:37:34 +0600

    update

--- node/async/index.js
+++ node/async/index.js
@@ -1,13 +1,42 @@
 const fs=require('fs');
 
-// new file creation
-fs.writeFile('file.txt','This is new Line',(err)=>{
-    console.log("New file has been created");
+ // this line will be printed first as async
+
+// creating new folder
+fs.mkdir('new_folder',(err)=>{
+    console.log("New Folder Has been created")
+})
+
+//delete a folder
+fs.rmdir('new_folder',(err)=>{
+    console.log("The folder has been deleted");
+})
+
+// creating new file
+fs.writeFile('new_file','cool text is written here',(err)=>{
+    console.log("a file and few text has been written");
 })
 
 
-// how to read file with call-back
+// deleting the file
+fs.unlink('new_file',(err)=>{
+    console.log("the file has been deleted");
+})
+
+
+// file is being read
 fs.readFile('file.txt','utf-8',(err,data)=>{
     console.log(data);
 })
-console.log("Printing") // this line will be printed first as async
+console.log("Printing")
+
+
+// setting new name
+fs.rename('file.txt','new_file_name.txt',(err)=>{
+    console.log("New name set")
+})
+
+// add some text to file
+fs.appendFile('new_file_name.txt','Here is new line',(err)=>{
+    console.log("new line has been added")
+})
--- node/path/index.js
+++ node/path/index.js
@@ -2,21 +2,21 @@ const path=require('path');
 
 // directory name
 var directoryName=path.dirname('G:/Web_Dev_Final_Projects/node-express-framework/path/index.js');
-console.log(directoryName);
+console.log("Dir name : "+directoryName);
 
 
 // file extension name
 var extensionName=path.extname('G:/Web_Dev_Final_Projects/node-express-framework/path/index.js');
-console.log(extensionName);
+console.log("File Extension name : "+extensionName);
 
 
 //file basename
 var baseName=path.basename('G:/Web_Dev_Final_Projects/node-express-framework/path/index.js');
-console.log(baseName);
+console.log("BaseName : "+baseName);
 
 
 //parsing and object
 var info=path.parse('G:/Web_Dev_Final_Projects/node-express-framework/path/index.js');
-console.log(info.name);
+console.log(info);
 
  
