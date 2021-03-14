ad8481a54630d9ccd6b1a783869e39247e8284af
zahid-bracu <zahid.mym@gmail.com>
2021-03-14 16:59:42 +0600

    update

--- express/templateEngine/index.js
+++ express/templateEngine/index.js
@@ -1,19 +1,23 @@
 const express=require('express'); 
 const path=require('path');
-const hbs=require('hbs');
+const hbs=require('hbs'); //hbs import
 const app=express(); 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000; //port set
 
+app.use(express.static(__dirname+'/public'));
 
-
+// path set
 const viewsPath=path.join(__dirname,'./templates/views');
 const partialsPath=path.join(__dirname,'./templates/partials');
 
-
-app.set('view engine', 'hbs');
-
+// path connect
 app.set('views',viewsPath);
 hbs.registerPartials(partialsPath);
+
+// hbs set
+app.set('view engine', 'hbs');
+
+
 
 // dynamic components
 app.get("/",(req,res)=>{
@@ -24,12 +28,15 @@ app.get("/",(req,res)=>{
 
 
 app.get("/about",(req,res)=>{
+
+    // props send
     res.render('about',{
         placeholder:"Md. Zahidur Rahman"
     })
 })
 
 
+// wrong page set
 app.get("*",(req,res)=>{
     res.render("404",{
         errorMessage:"The page is not found",
